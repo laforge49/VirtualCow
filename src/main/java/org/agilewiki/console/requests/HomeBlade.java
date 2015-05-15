39cae170226cdff00548ff90403b79388a3d8e67
Bill La Forge <laforge49@gmail.com>
2015-05-15 10:52:48 -0400

    dropped time navigation from home page

--- src/main/java/org/agilewiki/console/requests/HomeBlade.java
+++ src/main/java/org/agilewiki/console/requests/HomeBlade.java
@@ -26,27 +26,11 @@ public class HomeBlade extends RequestBlade {
             protected void process()
                     throws Exception {
                 String timestamp = request.getParameter("timestamp");
-                String dateInString = request.getParameter("date");
-                if (dateInString != null && dateInString.length() > 0) {
-                    Date date;
-                    SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
-                    try {
-                        date = formatter.parse(dateInString);
-                    } catch (ParseException e) {
-                        throw new ServletException(e);
-                    }
-                    GregorianCalendar calendar = new GregorianCalendar();
-                    calendar.setTime(date);
-                    calendar.set(Calendar.SECOND, 59);
-                    long time = calendar.getTimeInMillis() + 999;
-                    timestamp = TimestampIds.value(TimestampIds.timestampId((time << 10) + 1023));
-                }
                 if (timestamp == null) {
                     map.put("post", "post");
                 } else {
                     map.put("setTimestamp", "&timestamp=" + timestamp);
                     map.put("atTime", "at " + SimpleSimon.niceTime(TimestampIds.generate(timestamp)));
-                    map.put("clearTime", "<a href=\".\">Clear selected time</a>");
                 }
                 finish();
             }
