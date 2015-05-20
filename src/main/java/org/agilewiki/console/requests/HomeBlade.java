686018a94a1513acb488208696a3c2326bbc30bf
Bill La Forge <laforge49@gmail.com>
2015-05-20 11:33:02 -0400

    rupa rupa in the profile, in the home

--- src/main/java/org/agilewiki/console/requests/HomeBlade.java
+++ src/main/java/org/agilewiki/console/requests/HomeBlade.java
@@ -2,6 +2,8 @@ package org.agilewiki.console.requests;
 
 import org.agilewiki.console.SimpleSimon;
 import org.agilewiki.console.TimestampIds;
+import org.agilewiki.console.User;
+import org.agilewiki.utils.immutable.FactoryRegistry;
 
 import javax.servlet.AsyncContext;
 
@@ -19,6 +21,8 @@ public class HomeBlade extends RequestBlade {
             @Override
             protected void process()
                     throws Exception {
+                String myEmail = User.email(db, userId, FactoryRegistry.MAX_TIMESTAMP);
+                map.put("myEmail", myEmail);
                 String timestamp = request.getParameter("timestamp");
                 if (timestamp == null) {
                     map.put("post", "post");
