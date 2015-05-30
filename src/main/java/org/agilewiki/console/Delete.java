72b145ccfac9d66d4857423b050627337b94e299
Bill La Forge <laforge49@gmail.com>
2015-05-30 07:20:13 -0400

    delete now deletes directed links

--- src/main/java/org/agilewiki/console/Delete.java
+++ src/main/java/org/agilewiki/console/Delete.java
@@ -1,5 +1,6 @@
 package org.agilewiki.console;
 
+import org.agilewiki.utils.ids.composites.Link1Id;
 import org.agilewiki.utils.ids.composites.SecondaryId;
 import org.agilewiki.utils.virtualcow.Db;
 
@@ -12,6 +13,16 @@ public class Delete {
         for (String secondaryType : SecondaryId.typeIdIterable(db, id)) {
             for (String secondaryId : SecondaryId.secondaryIdIterable(db, id, secondaryType, db.getTimestamp())) {
                 SecondaryId.removeSecondaryId(db, id, secondaryId);
+            }
+        }
+        for (String lnkTyp: Link1Id.link1LabelIdIterable(db, id)) {
+            for (String tId: Link1Id.link1IdIterable(db, id, lnkTyp, db.getTimestamp())) {
+                Link1Id.removeLink1(db, id, lnkTyp, tId);
+            }
+        }
+        for (String lnkTyp: Link1Id.link1LabelInvIterable(db, id)) {
+            for (String oId: Link1Id.link1InvIterable(db, id, lnkTyp, db.getTimestamp())) {
+                Link1Id.removeLink1(db, oId, lnkTyp, id);
             }
         }
     }
