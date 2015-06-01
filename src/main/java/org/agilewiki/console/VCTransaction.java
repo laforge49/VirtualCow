e1a943aec92b5db7b782fdac7669f8c642ac5622
Bill La Forge <laforge49@gmail.com>
2015-06-01 07:26:12 -0400

    transactionName secondary key

--- src/main/java/org/agilewiki/console/VCTransaction.java
+++ src/main/java/org/agilewiki/console/VCTransaction.java
@@ -1,6 +1,8 @@
 package org.agilewiki.console;
 
+import org.agilewiki.utils.ids.NameId;
 import org.agilewiki.utils.ids.composites.Link1Id;
+import org.agilewiki.utils.ids.composites.SecondaryId;
 import org.agilewiki.utils.immutable.collections.MapNode;
 import org.agilewiki.utils.virtualcow.Db;
 import org.agilewiki.utils.virtualcow.Transaction;
@@ -11,6 +13,10 @@ import org.agilewiki.utils.virtualcow.Transaction;
 public abstract class VCTransaction implements Transaction {
     @Override
     public final void transform(Db db, MapNode tMapNode) {
+        String transactionName = tMapNode.get(Db.transactionNameId).toString();
+        String secondaryId = SecondaryId.secondaryId(Db.transactionNameId,
+                NameId.generate(transactionName));
+        SecondaryId.createSecondaryId(db, db.getJEName(), secondaryId);
         String userId = (String) tMapNode.get(User.USER_KEY);
         if (userId != null) {
             String jeName = db.getJEName();
