1b0ec1b6237beb413b98e1e437d4a76549858b54
Bill La Forge <laforge49@gmail.com>
2015-06-07 16:24:29 -0400

    secondary index on .node

--- src/main/java/org/agilewiki/console/RecreateSystemRoleTransaction.java
+++ src/main/java/org/agilewiki/console/RecreateSystemRoleTransaction.java
@@ -41,17 +41,28 @@ public class RecreateSystemRoleTransaction extends RecreateRoleTransaction {
         SecondaryId.createSecondaryId(db, "$n.node.link1",  SecondaryId.secondaryId("$ninvDependent", "$n.link1"));
 
         Link1Id.createLink1(db, "$nmetadata.node", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$nmetadata.node", SecondaryId.secondaryId("$n.node", "$nmetadata.node"));
         Link1Id.createLink1(db, "$n.node", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$n.node", SecondaryId.secondaryId("$n.node", "$n.node"));
         Link1Id.createLink1(db, "$n.role", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$n.role", SecondaryId.secondaryId("$n.node", "$n.role"));
         Link1Id.createLink1(db, "$n.journalEntry", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$n.journalEntry", SecondaryId.secondaryId("$n.node", "$n.journalEntry"));
         Link1Id.createLink1(db, "$n.link1", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$n.link1", SecondaryId.secondaryId("$n.node", "$n.link1"));
         Link1Id.createLink1(db, "$n.keyType", "$n.nodeType", "$nmetadata.node");
+        SecondaryId.createSecondaryId(db, "$n.keyType", SecondaryId.secondaryId("$n.node", "$n.keyType"));
 
         Link1Id.createLink1(db, "$nuser.link1", "$n.nodeType", "$n.link1");
+        SecondaryId.createSecondaryId(db, "$nuser.link1", SecondaryId.secondaryId("$n.node", "$nuser.link1"));
         Link1Id.createLink1(db, "$n.role.link1", "$n.nodeType", "$n.link1");
+        SecondaryId.createSecondaryId(db, "$n.role.link1", SecondaryId.secondaryId("$n.node", "$n.role.link1"));
         Link1Id.createLink1(db, "$n.link1.link1", "$n.nodeType", "$n.link1");
+        SecondaryId.createSecondaryId(db, "$n.link1.link1", SecondaryId.secondaryId("$n.node", "$n.link1.link1"));
         Link1Id.createLink1(db, "$n.node.link1", "$n.nodeType", "$n.link1");
+        SecondaryId.createSecondaryId(db, "$n.node.link1", SecondaryId.secondaryId("$n.node", "$n.node.link1"));
         Link1Id.createLink1(db, "$n.nodeType.link1", "$n.nodeType", "$n.link1");
+        SecondaryId.createSecondaryId(db, "$n.nodeType.link1", SecondaryId.secondaryId("$n.node", "$n.nodeType.link1"));
 
         Link1Id.createLink1(db, "$nsubject.keyType", "$n.keyType", "$nsystem.role");
         Link1Id.createLink1(db, "$nsubject.keyType", "$n.nodeType", "$n.keyType");
@@ -65,5 +76,7 @@ public class RecreateSystemRoleTransaction extends RecreateRoleTransaction {
         Link1Id.createLink1(db, "$n.link1.keyType", "$n.nodeType", "$n.keyType");
         Link1Id.createLink1(db, "$ninvDependent.keyType", "$n.keyType", "$nsystem.role");
         Link1Id.createLink1(db, "$ninvDependent.keyType", "$n.nodeType", "$n.keyType");
+        Link1Id.createLink1(db, "$n.node", "$n.keyType", "$nsystem.role");
+        Link1Id.createLink1(db, "$n.node", "$n.nodeType", "$n.keyType");
     }
 }
