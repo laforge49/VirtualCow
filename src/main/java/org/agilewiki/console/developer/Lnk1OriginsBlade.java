993a1a50d710fea5578bb06bc5754b7c9cfa3ecc
Bill La Forge <laforge49@gmail.com>
2015-06-12 10:24:04 -0400

    cleanup

--- src/main/java/org/agilewiki/console/developer/Lnk1OriginsBlade.java
+++ src/main/java/org/agilewiki/console/developer/Lnk1OriginsBlade.java
@@ -6,10 +6,8 @@ import org.agilewiki.console.Role;
 import org.agilewiki.console.SimpleSimon;
 import org.agilewiki.utils.ids.NameId;
 import org.agilewiki.utils.ids.composites.Link1Id;
-import org.agilewiki.utils.immutable.collections.ListAccessor;
 import org.agilewiki.utils.immutable.collections.MapAccessor;
 import org.agilewiki.utils.immutable.collections.PeekABoo;
-import org.agilewiki.utils.immutable.collections.VersionedMapNode;
 import org.agilewiki.utils.virtualcow.UnexpectedChecksumException;
 
 import javax.servlet.AsyncContext;
@@ -59,63 +57,25 @@ public class Lnk1OriginsBlade extends RequestBlade {
                         PeekABoo<String> idPeekABoo = Link1Id.label1IdIterable(db, labelId, longTimestamp);
                         if (startingAt != null)
                             idPeekABoo.setPosition(startingAt);
-                        for (String id : idPeekABoo) {
+                        for (String nodeId : idPeekABoo) {
                             if (limit == 0) {
                                 hasMore = true;
                                 startingAt = idPeekABoo.getPosition();
                                 break;
                             }
                             --limit;
-                            String label1IndexId = Link1Id.label1IndexId(NameId.generate(Lnk1OriginsBlade.this.label), id);
                             MapAccessor ma = db.mapAccessor();
-                            ListAccessor la = ma.listAccessor(label1IndexId);
-                            String nodeId = null;
-                            if (la != null) {
-                                VersionedMapNode vmn = (VersionedMapNode) la.get(0);
-                                if (vmn != null) {
-                                    String first = (String) vmn.firstKey(longTimestamp);
-                                    String last = (String) vmn.lastKey(longTimestamp);
-                                    if (first.equals(last)) {
-                                        nodeId = first;
-                                    }
-                                }
-                            }
-                            String line = id;
-                            if (line.length() > 60)
-                                line = line.substring(0, 60);
-                            line = SimpleSimon.encode(line, 0, SimpleSimon.ENCODE_SINGLE_LINE); //line text
-                            if (nodeId == null) {
-                                sb.append("<a href=\"?from=");
-                                sb.append(page);
-                                sb.append("&to=nodes&secondaryId=");
-                                sb.append(label1IndexId);
-                                if (timestamp != null) {
-                                    sb.append("&timestamp=");
-                                    sb.append(timestamp);
-                                }
-                                sb.append(setRole + "#rupa\">");
-                                sb.append(line);
-                                sb.append("</a>");
-                            } else {
-                                sb.append(line);
-                                sb.append(" -> ");
-                                sb.append("<a href=\"?from=");
-                                sb.append(page);
-                                sb.append("&to=node&nodeId=");
-                                sb.append(nodeId);
-                                if (timestamp != null) {
-                                    sb.append("&timestamp=");
-                                    sb.append(timestamp);
-                                }
-                                sb.append(setRole + "#rupa\">");
-                                sb.append(nodeId);
-                                sb.append("</a>");
-                            }
-                            sb.append(", <a href=\"?from=");
+                            sb.append("<a href=\"?from=");
                             sb.append(page);
-                            sb.append("&to=subJournal&subJournal=");
-                            sb.append(label1IndexId + setRole);
-                            sb.append("#rupa\">journal</a>");
+                            sb.append("&to=node&nodeId=");
+                            sb.append(nodeId);
+                            if (timestamp != null) {
+                                sb.append("&timestamp=");
+                                sb.append(timestamp);
+                            }
+                            sb.append(setRole + "#rupa\">");
+                            sb.append(nodeId);
+                            sb.append("</a>");
                             sb.append("<br />");
                         }
                         break;
