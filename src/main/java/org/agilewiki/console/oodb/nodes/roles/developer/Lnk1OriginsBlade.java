6c9ba309acf926f6155e4a0ea950d14d7ccc7de9
Bill La Forge <laforge49@gmail.com>
2015-06-17 19:46:38 -0400

    origins

--- src/main/java/org/agilewiki/console/oodb/nodes/roles/developer/Lnk1OriginsBlade.java
+++ src/main/java/org/agilewiki/console/oodb/nodes/roles/developer/Lnk1OriginsBlade.java
@@ -65,19 +65,13 @@ public class Lnk1OriginsBlade extends RequestBlade {
                             }
                             --limit;
                             MapAccessor ma = db.mapAccessor();
+
                             String kindId = SecondaryIds.kindId(db, nodeId, longTimestamp);
-                            sb.append("<a href=\"?from=");
-                            sb.append(page);
-                            sb.append("&to=node&nodeId=");
-                            sb.append(kindId);
-                            if (timestamp != null) {
-                                sb.append("&timestamp=");
-                                sb.append(timestamp);
-                            }
-                            sb.append(setRole + "#rupa\">");
                             sb.append(kindId.substring(2));
-                            sb.append("</a>");
-                            sb.append(" <a href=\"?from=");
+
+                            sb.append(' ');
+
+                            sb.append("<a href=\"?from=");
                             sb.append(page);
                             sb.append("&to=node&nodeId=");
                             sb.append(nodeId);
