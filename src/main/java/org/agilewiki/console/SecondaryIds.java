85cb5595580d8ba1fd858b286833be900852db5b
Bill La Forge <laforge49@gmail.com>
2015-06-26 01:58:44 -0400

    simplified SecondaryIds

--- src/main/java/org/agilewiki/console/SecondaryIds.java
+++ src/main/java/org/agilewiki/console/SecondaryIds.java
@@ -13,36 +13,8 @@ import java.util.List;
  * Adds methods to SecondaryId.
  */
 public class SecondaryIds extends SecondaryId {
-    public static boolean hasSecondaryType(Db db, String vmlId, String typeId, long timestamp) {
-        String inv = secondaryInv(vmlId, typeId);
-        MapAccessor ma = db.mapAccessor();
-        ListAccessor la = ma.listAccessor(inv);
-        if (la == null)
-            return false;
-        VersionedMapNode invn = (VersionedMapNode) la.get(0);
-        if (invn == null)
-            return false;
-        return !invn.isEmpty(timestamp);
-    }
-
-    public static boolean nodeIsA(Db db, String nodeId, String metaNodeId, long longTimestamp) {
-        if (nodeId.equals(metaNodeId))
-            return true;
-        for (String mnId : SimpleSimon.simpleSimon.ooDb.secondaryIdIterable(nodeId, Key_Node.NODETYPE_ID, longTimestamp)) {
-            if (nodeIsA(db, mnId, metaNodeId, longTimestamp))
-                return true;
-        }
-        return false;
-    }
-
     public static boolean isNode(Db db, String id, long longTimestamp) {
-        char idtyp = id.charAt(1);
-        if (!Character.isLowerCase(idtyp) || idtyp == 'v')
-            return false;
-        MapAccessor ma = db.mapAccessor();
-        String secInv = SecondaryId.secondaryInv(id, Key_Node.NODETYPE_ID);
-        VersionedMapNode veln = (VersionedMapNode) ma.get(secInv);
-        return veln != null && !veln.isEmpty(longTimestamp);
+        return SimpleSimon.simpleSimon.ooDb.fetchNode(id) != null;
     }
 
     public static String nodeTypeId(Db db, String nodeId, long longTimestamp) {
